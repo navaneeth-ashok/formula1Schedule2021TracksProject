32b60ccd0fb7ed33848ebe42d06b2485f0f0423e
navaneeth_ashok_1 <navaneeth619@gmail.com>
2021-04-21 23:03:05 +0530

    first commit

---
+++ scripts/countdown.js
@@ -0,0 +1,86 @@
+startClock();
+$("#reset").attr("disabled", true);
+$("#reset").css("backgroundColor","grey");
+
+function raceCountDown(){
+    
+    var raceTimes = document.getElementsByClassName("race_time");
+    var myTimes = document.getElementsByClassName("my_time");
+    
+    for(var i=0; i < raceTimes.length; i++){
+        var raceTime = new Date(raceTimes[i].innerHTML);
+        var date = new Date(); 
+        var now_utc =  Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds());
+    //    console.log(raceTime-now_utc);
+        var seconds = Math.floor((raceTime-now_utc)/1000);
+        if (seconds > 1){
+            var minutes = Math.floor(seconds/60);
+            var hours = Math.floor(minutes/60);
+            var days = Math.floor(hours/24);
+
+            hours = hours-(days*24);
+            minutes = minutes-(days*24*60)-(hours*60);
+            seconds = seconds-(days*24*60*60)-(hours*60*60)-(minutes*60);
+            if (seconds < 10){
+                seconds = "0" + seconds;
+            }
+            myTimes[i].innerHTML = days + "d :" + hours +"h :" + minutes + "m :" + seconds + "s";
+            
+        } else {
+            myTimes[i].innerHTML = "Race completed";
+        }
+        
+        
+        
+    }
+    
+    
+    
+}
+
+
+function startClock(){
+    tickTock = setInterval(raceCountDown, 1000);
+}
+function showLocalTime(){
+    var raceTimes = document.getElementsByClassName("race_time");
+    var myTimes = document.getElementsByClassName("my_time");
+//    console.log(raceTimes.length);
+//    console.log($(".race_time"));
+//    for (r in raceTimes){
+//        console.log(r.innerHTML);
+//    }
+    for(var i=0; i < raceTimes.length; i++){
+        var raceTimeUTC = raceTimes[i].innerHTML;
+        console.log(raceTimeUTC);
+        raceTimeUTC = raceTimeUTC.toString() + " UTC";
+        var localDate = new Date(raceTimeUTC);
+        raceTimes[i].innerHTML= localDate;
+        $("heading-time").text("Race Start Time - Local");
+    }
+//    for ( racetime in raceTimes)
+//    var raceTimeUTC = $("#race_time").text();
+//    raceTimeUTC = raceTimeUTC.toString() + " UTC";
+//    var localDate = new Date(raceTimeUTC);
+//    $("#race_time").text(localDate);
+//    $("heading-time").text("Race Start Time - Local");
+}
+
+$("#localTime").click(function(){
+    showLocalTime();
+    $("#localTime").attr("disabled", true);
+    $("#localTime").css("backgroundColor","grey");
+    $("#reset").css("backgroundColor","red");
+    $("#reset").attr("disabled", false);
+});
+
+
+
+$("#reset").click(function() {
+    location.reload();
+});
+
+var divClone = $("#document").clone(true);
+
+
+
---
+++ scripts/script.js
@@ -0,0 +1,73 @@
+// Track objects for storing the information
+var track1_obj = {
+    trackTitle: "1. Monza, Italy",
+    trackImage : "images/monza.png",
+    trackDesc : "<p>Since the start of the world championship era, no circuit has hosted more grands prix than Monza.</p><p>Located in the city of the same name, a few miles north of Milan, this track started life in 1922. The layout back then incorporated a banked oval, but the rest was almost identical to the circuit we see today.</p><p>Chicanes have been added and some turns have been slightly re-profiled, but drivers of every era negotiated corners such as Curva Grande, the Lesmos and Parabolica.</p><p>And if the history wasn't enough, Monza is also the spiritual home of the tifosi, who make up one of F1's most passionate, knowledgeable crowds.</p><p>It doesn't match up to a circuit like Spa in terms of sheer beauty and challenge, but Monza is still a worthy No. 1</p>",
+    trackStats:"<table class='pure-table is-hoverable'> <thead> <tr> <th>Date</th> <th>Grand Prix</th> <th>Pole Position</th> <th>Time</th> <th>Win</th> <th>Time</th> <th>Fastest Lap</th> <th>Time</th> </tr></thead> <tbody> <tr> <td>4 september 2016</td><td>Italy</td><td>Lewis Hamilton</td><td>1'21''135</td><td>Nico Roseberg</td><td>1h 17m 28.089s</td><td>Fernando Alonso</td><td>1'25''340</td></tr><tr> <td>3 september 2017</td><td>Italy</td><td>Lewis Hamilton</td><td>1'35''554</td><td>Lewis Hamilton</td><td>1h 15m 32.312s</td><td>Daniel Ricciardo</td><td>1'23''361</td></tr><tr> <td>2 september 2018</td><td>Italy</td><td>Kimi Raikkonen</td><td>1'19''119</td><td>Lewis Hamilton</td><td>1h 16m 54.484s</td><td>Lewis Hamilton</td><td>1'22''497</td></tr><tr> <td>8 september 2019</td><td>Italy</td><td>Charles Leclerc</td><td>1'19''307</td><td>Charles Leclerc</td><td>1h 15m 26.665s</td><td>Lewis Hamilton</td><td>1'21''779</td></tr><tr> <td>6 september 2020</td><td>Italy</td><td>Lewis Hamilton</td><td>1'18''887</td><td>Pierre Gasly</td><td>1h 47m 06.056s</td><td>Lewis Hamilton</td><td>1'22''746</td></tr></tbody> </table>"
+}
+
+var track2_obj = {
+    trackTitle: "2. Circuit De Monaco, Monaco",
+    trackImage : "images/monaco.png",
+    trackDesc : "<p>Held around the streets of the world's second-smallest country, the Monaco Grand Prix has taken place 61 times in the world championship era.</p><p>It's the last true, old-style street circuit in F1. The roads it runs on were designed primarily for everyday traffic, with little consideration given to the sport. And it shows—the circuit is totally unsuitable for the cars of today.</p><p>There's little room for modern safety features, the straights aren't long enough and races are usually dull processions.</p><p>But it's allowed to exist simply because it's Monaco. It has glamour, prestige and (perhaps more importantly these days) everyone has heard of it, whether they know what F1 is or not.</p><p>And from a fan's point of view, when an overtake does occur, it's usually pretty special.</p>",
+    trackStats:"<table class='pure-table is-hoverable'> <thead> <tr> <th>Date</th> <th>Grand Prix</th> <th>Pole Position</th> <th>Time</th> <th>Win</th> <th>Time</th> <th>Fastest Lap</th> <th>Time</th> </tr></thead> <tbody> <tr> <td>24 may 2015</td><td>Monaco</td><td>Lewis Hamilton</td><td>1'15''098</td><td>Nico Roseberg</td><td>1h 49m 18.420s</td><td>Daniel Ricciardo</td><td>1'18''063</td></tr><tr> <td>29 may 2016</td><td>Monaco</td><td>Daniel Ricciardo</td><td>1'13''622</td><td>Lewis Hamilton</td><td>1h 59m 29.133s</td><td>Lewis Hamilton</td><td>1'17''939</td></tr><tr> <td>28 may 2017</td><td>Monaco</td><td>Kimi Raikkonen</td><td>1'12''178</td><td>Sebastian Vetel</td><td>1h 44m 44.340s</td><td>Sergio Perez</td><td>1'14''820</td></tr><tr> <td>27 may 2018</td><td>Monaco</td><td>Daniel Ricciardo</td><td>1'10''810</td><td>Daniel Ricciardo</td><td>1h 42m 54.807s</td><td>Max Verstappen</td><td>1'14''260</td></tr><tr> <td>26 may 2019</td><td>Monaco</td><td>Lewis Hamilton</td><td>1'10''166</td><td>Lewis Hamilton</td><td>1h 43m 28.437s</td><td>Pierre Gasly</td><td>1'14''279</td></tr></tbody> </table>"
+}
+
+var track3_obj = {
+    trackTitle: "3. Spa-Francorchamps, Belgium",
+    trackImage : "images/spa.png",
+    trackDesc : "<p>The modern Spa-Francorchamps circuit bears only a passing resemblance to the fearsome old beast that once hosted grands prix.</p><p>But unlike so many other cut-down modern tracks, Spa managed to retain its charm and majesty.</p><p>The old layout was a 14.1-kilometre (8.8-mile) blast around the Belgian countryside. It was always dangerous, and as F1 moved forward the circuit was left behind. Old Spa hosted its final F1 race in 1970.</p><p>New Spa made its F1 debut in 1983. A smaller circuit, it retained famous corners such as Eau Rouge, Blanchimont and La Source while adding the quite lovely Pouhon.</p><p>Increasing downforce levels have made once-difficult corners easy, but Spa is still the best F1-standard circuit in the world.</p>",
+    trackStats:"<table class='pure-table is-hoverable'> <thead> <tr> <th>Date</th> <th>Grand Prix</th> <th>Pole Position</th> <th>Time</th> <th>Win</th> <th>Time</th> <th>Fastest Lap</th> <th>Time</th> </tr></thead> <tbody> <tr> <td>24 may 2015</td><td>Monaco</td><td>Lewis Hamilton</td><td>1'15''098</td><td>Nico Roseberg</td><td>1h 49m 18.420s</td><td>Daniel Ricciardo</td><td>1'18''063</td></tr><tr> <td>29 may 2016</td><td>Monaco</td><td>Daniel Ricciardo</td><td>1'13''622</td><td>Lewis Hamilton</td><td>1h 59m 29.133s</td><td>Lewis Hamilton</td><td>1'17''939</td></tr><tr> <td>28 may 2017</td><td>Monaco</td><td>Kimi Raikkonen</td><td>1'12''178</td><td>Sebastian Vetel</td><td>1h 44m 44.340s</td><td>Sergio Perez</td><td>1'14''820</td></tr><tr> <td>27 may 2018</td><td>Monaco</td><td>Daniel Ricciardo</td><td>1'10''810</td><td>Daniel Ricciardo</td><td>1h 42m 54.807s</td><td>Max Verstappen</td><td>1'14''260</td></tr><tr> <td>26 may 2019</td><td>Monaco</td><td>Lewis Hamilton</td><td>1'10''166</td><td>Lewis Hamilton</td><td>1h 43m 28.437s</td><td>Pierre Gasly</td><td>1'14''279</td></tr></tbody> </table>"
+}
+
+var track4_obj = {
+    trackTitle: "4. Silverstone, United Kingdom",
+    trackImage : "images/silverstone.png",
+    trackDesc : "<p>Silverstone hosted the first-ever race of the world championship era, all the way back in May 1950. Since then it has held the British Grand Prix 47 times.</p><p>The land upon which the circuit lies started life as RAF Silverstone, a training base for British air crews flying the Wellington bomber. Opened in 1943, it was used until the end of World War II.</p><p>When hostilities ceased the aircraft departed and the airfield's perimeter road was put into service as a race track.</p><p>Silverstone's overall shape has changed little, but numerous corners and chicanes have been added and changed over the years, mostly in the name of safety.</p><p></p>Fortunately, the character of the place has always been retained, and the Maggots-Becketts-Chapel complex stands out as one of the best quick corner sequences in the world.<p>Fast and flowing, Silverstone remains a favourite of drivers and fans alike.</p>",
+    trackStats:"<table class='pure-table is-hoverable'> <thead> <tr> <th>Date</th> <th>Grand Prix</th> <th>Pole Position</th> <th>Time</th> <th>Win</th> <th>Time</th> <th>Fastest Lap</th> <th>Time</th> </tr></thead> <tbody> <tr> <td>16 july 2017</td><td>Britain</td><td>Lewis Hamilton</td><td>1'26''600</td><td>Lewis Hamilton</td><td>1h 21m 27.430s</td><td>Lewis Hamilton</td><td>1'30''621</td></tr><tr> <td>8 july 2018</td><td>Britain</td><td>Lewis Hamilton</td><td>1'25''892</td><td>Sebastian Vetel</td><td>1h 27m 29.784s</td><td>Sebastian Vetel</td><td>1'30''696</td></tr><tr> <td>14 july 2019</td><td>Britain</td><td>Vallteri Bottas</td><td>1'25''093</td><td>Lewis Hamilton</td><td>1h 21m 08.452s</td><td>Lewis Hamilton</td><td>1'27''369</td></tr><tr> <td>2 august 2020</td><td>Britain</td><td>Lewis Hamilton</td><td>1'24''303</td><td>Lewis Hamilton</td><td>1h 28m 01.283s</td><td>Max Verstappen</td><td>1'27''097</td></tr><tr> <td>9 august 2020</td><td>70th Anniversary</td><td>Vallteri Bottas</td><td>1'25''154</td><td>Max Verstappen</td><td>1h 19m 41.996s</td><td>Lewis Hamilton</td><td>1'28''451</td></tr></tbody> </table>"
+}
+
+var track5_obj = {
+    trackTitle: "5. Nurburgring, Germany",
+    trackImage : "images/nurburgring.png",
+    trackDesc : "<p>In the early days of F1, it wasn't uncommon for races to be run on public roads. Such circuits tended to be long, fast and dangerous—and the Granddaddy of the them all was the Nurburgring's Nordschleife (northern loop).</p><p>Over 22 kilometres (14 miles) in length, the Nordschleife featured jumps, banking, blind crests and 160 corners. F1 has never seen a greater test of man and machine, and it never again will.</p><p>But with the challenge and sheer size of the place also came danger. As the sport progressed into the 1970s, the risk of racing here was deemed unacceptable. Changes were made to improve safety, but they weren't (and could never have been) enough.</p><p>The German Grand Prix of 1976—in which Niki Lauda was badly burned and almost lost his life—was the last F1 race held on the Nordschleife.</p><p>In 1984 a new circuit hosted the German Grand Prix, the Nurburgring GP-Strecke. Tacked onto the southern edge of the Nordschleife, the new circuit was built to conform to modern safety standards.</p><p>It was also a little bit sterile and bland. Changes have been made over the years in an attempt to improve the spectacle and it's better than it was, but the GP-Strecke will never match up to its big brother.</p>",
+    trackStats:"<table class='pure-table is-hoverable'> <thead> <tr> <th>Date</th> <th>Grand Prix</th> <th>Pole Position</th> <th>Time</th> <th>Win</th> <th>Time</th> <th>Fastest Lap</th> <th>Time</th> </tr></thead> <tbody> <tr> <td>22 july 2007</td><td>Germany</td><td>Kimi Raikkonen</td><td>1'31''450</td><td>Fernando Alonso</td><td>2h 06m 26.358s</td><td>Felipe Massa</td><td>1'32''853</td></tr><tr> <td>12 july 2009</td><td>Germany</td><td>Mark Webber</td><td>1'32''230</td><td>Mark Webber</td><td>1h 36m 43.310s</td><td>Fernando Alonso</td><td>1'33''365</td></tr><tr> <td>24 july 2011</td><td>Germany</td><td>Mark Webber</td><td>1'30''079</td><td>Lewis Hamilton</td><td>1h 37m 30.334s</td><td>Lewis Hamilton</td><td>1'34''302</td></tr><tr> <td>7 july 2013</td><td>Germany</td><td>Lewis Hamilton</td><td>1'29''398</td><td>Sebastian Vettel</td><td>1h 41m 14.711s</td><td>Fernando Alonso</td><td>1'33''468</td></tr><tr> <td>11 october 2020</td><td>Eifel</td><td>Vallteri Bottas</td><td>1'25''269</td><td>Lewis Hamilton</td><td>1h 35m 49.641s</td><td>Max Verstappen</td><td>1'28''139</td></tr></tbody> </table>"
+}
+
+// save the objects in to an array of objects
+var tracks = [track1_obj, track2_obj, track3_obj, track4_obj, track5_obj];
+
+// function to enlarge the track when clicked
+function enlargeImage(id){
+    console.log("Enlarging the image");
+    $('#' + id).toggleClass("enlarged");
+    $('#' + id).draggable;
+    
+}
+
+// carousel function to scroll the entire divs
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    touchDrag: false,
+    mouseDrag: false,
+    navText: ["<img src='images/arrow_back.svg'>","<img src='images/arrow_forward.svg'>"],
+    responsive:{
+        0:{
+            items:1
+        }
+    }
+})
+
+
+// read from the array of objects and fill in the DOM
+var i = 1;
+for ( track of tracks){
+    $("h2#track_" + i + "_id").text(track.trackTitle);
+    $("img#tr" + i + "_img").attr("src",track.trackImage);
+    $("div#track_" + i + "_description").html(track.trackDesc);
+    $("div#track_" + i + "_stats").html(track.trackStats);
+    i ++;
+};
+
